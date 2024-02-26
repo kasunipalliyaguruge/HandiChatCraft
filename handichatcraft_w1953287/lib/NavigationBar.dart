4b3c1d13f75fe7efcb757098cef5047b2a6ab157
kasunipalliyaguruge <148673909+kasunipalliyaguruge@users.noreply.github.com>
2024-02-26 08:10:07 +0530

    forth commit

---
+++ handichatcraft_w1953287/lib/NavigationBar.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: NotificationView(),
+    );
+  }
+}
+
+class NotificationView extends StatefulWidget {
+  @override
+  _NotificationViewState createState() => _NotificationViewState();
+}
+
+class _NotificationViewState extends State<NotificationView> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notification Screen'),
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: Center(
+        child: Text('Your main content goes here'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.orangeAccent,
+                  type: BottomNavigationBarType.fixed, // Set to fixed to display all items
+
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.reviews),
+            label: 'Feedback',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chatbot',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        selectedItemColor: Colors.white, // Change the color as needed
+          unselectedItemColor: Colors.black,
+          selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+          unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
+      ),
+    );
+  }
+}
