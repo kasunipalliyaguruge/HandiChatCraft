ebc099a2d7fd6c9c8273140c9fbb5254325750e1
Sathnara20221322 <158179152+Sathnara20221322@users.noreply.github.com>
2024-03-03 09:30:20 +0530

    first commit

--- handichatcraft_w1956102/lib/main.dart
+++ handichatcraft_w1956102/lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:handichatcraft_w1956102/communitypage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,7 +16,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       title: 'Community Page',
       debugShowCheckedModeBanner: false,
       home: Communitypage(),
---
+++ handicraft_w1956102_chatbotpage/android/app/src/main/kotlin/com/example/handicraft_w1956102_chatbotpage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.handicraft_w1956102_chatbotpage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
