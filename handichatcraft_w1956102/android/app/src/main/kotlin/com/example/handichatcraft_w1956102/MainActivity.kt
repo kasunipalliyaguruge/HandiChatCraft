c3ef85298d83a73688f870818fe21c829e85af1f
Sathnara20221322 <158179152+Sathnara20221322@users.noreply.github.com>
2024-03-03 09:47:49 +0530

    second commit

--- handichatcraft_w1956102/android/app/src/main/kotlin/com/example/handichatcraft_w1956102/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.handichatcraft_w1956102
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ handichatcraft_w1956102_communitypage/android/app/src/main/kotlin/com/example/handichatcraft_w1956102_communitypage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.handichatcraft_w1956102_communitypage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
