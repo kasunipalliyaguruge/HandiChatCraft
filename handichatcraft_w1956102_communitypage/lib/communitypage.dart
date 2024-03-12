220c42e3e95a48e780aa76d079d838fdbd3ad2a0
Sathnara20221322 <158179152+Sathnara20221322@users.noreply.github.com>
2024-03-12 11:16:32 +0530

    forth commit

--- handichatcraft_w1956102_communitypage/lib/communitypage.dart
+++ handichatcraft_w1956102_communitypage/lib/communitypage.dart
@@ -21,6 +21,7 @@ class Communitypage extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
+          SizedBox(height: 20),
           _buildRectangle(
               text: "National Crafts Council - Click Here",
               onTap: () {
