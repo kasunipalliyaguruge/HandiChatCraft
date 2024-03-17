a699f96edaa78d327f5bf3a66098510e9e7e5420
Sathnara20221322 <158179152+Sathnara20221322@users.noreply.github.com>
2024-03-17 15:58:54 +0530

    sixth commit

--- handicraft_w1956102_chatbotpage/lib/main.dart
+++ handicraft_w1956102_chatbotpage/lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // ignore: deprecated_member_use
       useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
