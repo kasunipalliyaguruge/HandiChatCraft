7edf579193c709d0c1abc0c8d681c08cf80f0fcb
Sathnara20221322 <158179152+Sathnara20221322@users.noreply.github.com>
2024-03-18 13:34:22 +0530

    eleventh commit

--- handicraft_w1956102_chatbotpage/lib/chatbotpage.dart
+++ handicraft_w1956102_chatbotpage/lib/chatbotpage.dart
@@ -1,49 +1,84 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'package:dialog_flowtter/dialog_flowtter.dart';
+import 'package:handicraft_w1956102_chatbotpage/messages.dart';
 
-class Chatbotpage extends StatelessWidget{
-const Chatbotpage({super.key});
+class Chatbotpage extends StatefulWidget {
+  const Chatbotpage({super.key});
 
-    @override
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ChatbotpageState createState() => _ChatbotpageState();
+}
+
+class _ChatbotpageState extends State<Chatbotpage> {
+  late DialogFlowtter dialogFlowtter;
+  final TextEditingController _controller = TextEditingController();
+
+  List<Map<String, dynamic>> messages = [];
+
+  @override
+  void initState() {
+    DialogFlowtter.fromFile().then((instance) => dialogFlowtter = instance);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(    
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(115.0),
-        child: Container(
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 5,
-                blurRadius: 7,
-                offset: Offset(0, 3),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Handibot'),
+      ),
+      // ignore: avoid_unnecessary_containers
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(child: MessagesScreen(messages: messages)),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+              color: Color.fromARGB(255, 172, 159, 136),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: TextField(
+                    controller: _controller,
+                    style: TextStyle(color: Colors.white),
+                  )),
+                  IconButton(
+                      onPressed: () {
+                        sendMessage(_controller.text);
+                        _controller.clear();
+                      },
+                      icon: Icon(Icons.send))
+                ],
               ),
-            ],
-          ),
-      child: AppBar(
-         centerTitle: true,
-         title: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-           // ignore: prefer_const_literals_to_create_immutables
-           children: [
-             Image.asset(
-               'lib/icons/chat-bot.png',
-               height: 32,
-               width: 32,
-             ),
-             SizedBox(height: 10),
-             Text(
-             'Handibot',
-             style: TextStyle(
-             color: Color.fromARGB(222, 222, 133, 24),
-             fontWeight: FontWeight.bold,
-              ),
-             )
-           ],
-          )
-         ),
-       ),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  sendMessage(String text) async {
+    if (text.isEmpty) {
+      // ignore: avoid_print
+      print('Message is empty');
+    } else {
+      setState(() {
+        addMessage(Message(text: DialogText(text: [text])), true);
+      });
+
+      DetectIntentResponse response = await dialogFlowtter.detectIntent(
+          queryInput: QueryInput(text: TextInput(text: text)));
+      if (response.message == null) return;
+      setState(() {
+        addMessage(response.message!);
+      });
+    }
+  }
+
+  addMessage(Message message, [bool isUserMessage = false]) {
+    messages.add({'message': message, 'isUserMessage': isUserMessage});
   }
 }
--- handicraft_w1956102_chatbotpage/lib/main.dart
+++ handicraft_w1956102_chatbotpage/lib/main.dart
@@ -1,6 +1,7 @@
 // ignore: unused_import
 import 'package:dialog_flowtter/dialog_flowtter.dart';
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:handicraft_w1956102_chatbotpage/chatbotpage.dart';
 import 'package:device_preview/device_preview.dart';
 
