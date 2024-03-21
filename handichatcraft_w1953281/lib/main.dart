05224721e8a74b52c5a4cdd5108e537d4ebb3dc0
Navodya Yashodani <158179265+Navo28@users.noreply.github.com>
2024-03-21 13:59:47 +0530

    second commit

--- handichatcraft_w1953281/lib/main.dart
+++ handichatcraft_w1953281/lib/main.dart
@@ -2,40 +2,60 @@ import 'package:flutter/material.dart';
 
 //import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:device_preview/device_preview.dart';
+import 'account_setting_page.dart';
 import 'settings_page.dart';
-
+import 'theme/provider.dart';
+import 'update_profile.dart';
 //import 'profile/theme/dark_mode_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // Set BindingBase.debugZoneErrorsAreFatal to true before initializing bindings
+  BindingBase.debugZoneErrorsAreFatal = true;
+
+  // Initialize Flutter bindings
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     DevicePreview(
       enabled: true,
-      builder: (context) => const MyApp(),
+      builder: (context) => MyApp(),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      useInheritedMediaQuery: true,
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter ',
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => UiProvider()..init(),
+      child:
+          Consumer<UiProvider>(builder: (context, UiProvider notifier, child) {
+        return MaterialApp(
+          useInheritedMediaQuery: true,
+          locale: DevicePreview.locale(context),
+          builder: DevicePreview.appBuilder,
+          debugShowCheckedModeBanner: false,
+          themeMode: notifier.isDark ? ThemeMode.dark : ThemeMode.light,
 
-      // Profile page
+          //dark custom theme applied
+          darkTheme: notifier.isDark ? notifier.darkTheme : notifier.lightTheme,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+            useMaterial3: true,
+          ),
+      initialRoute:'/profile',
+      routes: {
+        '/profile': (context) => ProfilePage(),
+            '/editprofilepage': (context) => EditProfilePage(
+                  initialName: 'John Doe',
+                  onUpdateName: (String) {},
+                ),
+            '/accountsettingepage': (context) => AccountSettingPage(),
 
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
-        useMaterial3: true,
-      ),
-      home: SettingsPage(),
+      },
     );
   }
 }
