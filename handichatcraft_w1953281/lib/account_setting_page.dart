1da8358934ad15103306bad7cf5612174edd7adb
Navodya Yashodani <158179265+Navo28@users.noreply.github.com>
2024-03-24 09:17:38 +0530

    first commit

--- handichatcraft_w1953281/lib/account_setting_page.dart
+++ handichatcraft_w1953281/lib/account_setting_page.dart
@@ -1,29 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:handichatcraft_w1953281/language_page.dart';
-import 'package:handichatcraft_w1953281/notification_page.dart';
-import 'package:handichatcraft_w1953281/security_page.dart';
-import 'package:handichatcraft_w1953281/theme/dark_mode_page.dart';
+import 'language_page.dart';
+import 'notification_page.dart';
 
-//import 'package:handicraft_profile/profile/update_profile.dart';
-//import 'package:settings_screen/settings_screen.dart';
-
-import 'change_password_page.dart';
-import 'settings_page.dart';
+import 'security_page.dart';
+import 'theme/dark_mode_page.dart';
 
 class AccountSettingPage extends StatelessWidget {
-  const AccountSettingPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Setting'),
+        title: const Text(
+          'Settings',
+          style: TextStyle(
+            color: Color.fromARGB(154, 196, 70, 7),
+            fontFamily: 'Calistoga',
+          ),
+        ),
         leading: IconButton(
           onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const ProfilePage()));
+            Navigator.pop(context); // Go back to ProfilePage
           },
-          icon: const Icon(Icons.arrow_back_outlined),
+          icon: const Icon(
+            Icons.arrow_back_outlined,
+            color: Colors.orange,
+          ),
         ),
         leadingWidth: 100,
       ),
@@ -59,38 +61,7 @@ class AccountSettingPage extends StatelessWidget {
             ),
           ),
           Container(
-            margin: const EdgeInsets.only(left: 25, right: 25, top: 130),
-            child: TextButton(
-              style: TextButton.styleFrom(
-                padding: const EdgeInsets.all(20),
-                foregroundColor: const Color.fromARGB(223, 233, 105, 7),
-              ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => ChangePasswordPage()));
-              },
-              child: const Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.lock),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  SizedBox(width: 20),
-                  Expanded(
-                      child: Text(
-                    'Change Password',
-                    style: TextStyle(fontSize: 16),
-                  )),
-                  Icon(Icons.arrow_forward_ios),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 25, right: 25, top: 210),
+            margin: const EdgeInsets.only(left: 25, right: 25, top: 140),
             child: TextButton(
               style: TextButton.styleFrom(
                 padding: const EdgeInsets.all(20),
@@ -121,7 +92,7 @@ class AccountSettingPage extends StatelessWidget {
             ),
           ),
           Container(
-            margin: const EdgeInsets.only(left: 25, right: 25, top: 290),
+            margin: const EdgeInsets.only(left: 25, right: 25, top: 230),
             child: TextButton(
               style: TextButton.styleFrom(
                 padding: const EdgeInsets.all(20),
@@ -150,7 +121,7 @@ class AccountSettingPage extends StatelessWidget {
             ),
           ),
           Container(
-            margin: const EdgeInsets.only(left: 25, right: 25, top: 370),
+            margin: const EdgeInsets.only(left: 25, right: 25, top: 320),
             child: TextButton(
               style: TextButton.styleFrom(
                 padding: const EdgeInsets.all(20),
@@ -160,7 +131,7 @@ class AccountSettingPage extends StatelessWidget {
                 Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => const NotificationPage()));
+                        builder: (context) => NotificationPage()));
               },
               child: const Row(
                 mainAxisSize: MainAxisSize.min,
@@ -180,38 +151,8 @@ class AccountSettingPage extends StatelessWidget {
               ),
             ),
           ),
-          /*Container(
-            margin: const EdgeInsets.only(left: 25, right: 25, top: 450),
-            child: TextButton(
-              style: TextButton.styleFrom(
-                padding: const EdgeInsets.all(20),
-                foregroundColor: const Color.fromARGB(223, 233, 105, 7),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15)),
-                backgroundColor: Color.fromARGB(255, 255, 255, 255),
-              ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const AccountSettingPage()));
-              },
-              child: const Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.notifications),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  SizedBox(width: 20),
-                  Expanded(child: Text('Notification')),
-                  Icon(Icons.arrow_forward_ios),
-                ],
-              ),
-            ),
-          ),*/
           Container(
-            padding: const EdgeInsets.only(left: 50, top: 520),
+            padding: const EdgeInsets.only(left: 50, top: 510),
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(
                 minimumSize: const Size(280, 55),
@@ -219,7 +160,9 @@ class AccountSettingPage extends StatelessWidget {
                 shape: const RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(30))),
               ),
-              onPressed: () {},
+              onPressed: () {
+                signOut(context);
+              },
               child: const Text(
                 'Log Out',
                 style: TextStyle(color: Colors.white, fontSize: 20),
@@ -228,125 +171,17 @@ class AccountSettingPage extends StatelessWidget {
           ),
         ],
       ),
-      /*body: settingsScreen(
-        [
-          {
-            "title": "General",
-            "subtitle": "General settings",
-            "onTap": () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const SettingsPage()));
-            },
-            "leadingIcon": Icons.settings,
-          },
-          {
-            "title": "Security",
-            "subtitle": "Security settings",
-            "onTap": () {},
-            "leadingIcon": Icons.security,
-          },
-          {
-            "title": "Notifications",
-            "subtitle": "Notifications settings",
-            "onTap": () {},
-            "leadingIcon": Icons.notifications,
-          },
-          {
-            "title": "About",
-            "subtitle": "About settings",
-            "onTap": () {},
-            "leadingIcon": Icons.info,
-          },
-          {
-            "title": "Language",
-            "subtitle": "English",
-            "onTap": () {},
-            "leadingIcon": Icons.language,
-          },
-        ],
-      ),*/
-      /*floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.cyan,
-        foregroundColor: const Color.fromARGB(255, 221, 127, 12),
-        child: const Icon(Icons.chat_bubble),
-      ),
-      bottomNavigationBar: const BottomAppBar(
-          color: Colors.black,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(left: 10, bottom: 10),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(
-                      Icons.home,
-                      color: Colors.grey,
-                    ),
-                    Text(
-                      "Home",
-                      style: TextStyle(color: Colors.white),
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(right: 20, bottom: 20),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(
-                      Icons.home,
-                      color: Colors.grey,
-                    ),
-                    Text(
-                      "Home",
-                      style: TextStyle(color: Colors.white),
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(right: 20, top: 10, bottom: 10),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(
-                      Icons.home,
-                      color: Colors.grey,
-                    ),
-                    Text(
-                      "Home",
-                      style: TextStyle(color: Colors.white),
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(right: 20, top: 10, bottom: 10),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(
-                      Icons.home,
-                      color: Colors.grey,
-                    ),
-                    Text(
-                      "Home",
-                      style: TextStyle(color: Colors.white),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          )
-          ),*/
     );
+  }
+
+  void signOut(BuildContext context) async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      // Navigate to the sign-in screen after sign-out
+      Navigator.pushNamedAndRemoveUntil(context, '/welcome', (route) => false);
+    } catch (e) {
+      print('Error signing out: $e');
+      // Handle sign-out errors, if any
+    }
   }
 }
